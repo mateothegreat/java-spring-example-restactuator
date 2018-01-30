68771871c5a5a55d49b992a095771be2d6277eab
Matthew Davis <makaio.hiwalani@gmail.com>
2018-01-30 12:32:50 -0600

    initial commit

---
+++ src/main/java/hello/RESTActuatorApplication.java
@@ -0,0 +1,14 @@
+
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RESTActuatorApplication.class, args);
+    }
+
+} 
